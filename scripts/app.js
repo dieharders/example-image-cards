50625cd315411dd1aa82addd1ce5eada28a27140
Sorob Raissi <cybrobo@gmail.com>
2018-11-03 04:10:22 -0700

    Calculate the num total pages for search. Changed style of title plates. Added avatar image next to name.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,14 @@
 // Globals
 var maxWidth = '360px';
 var maxHeight = '240px';
+var currPage = 1;
+var totalPages = 100; // im guessing
 
-// Check when photo is loaded into DOM from server
+// Check when photo is loaded from server
 function imgLoaded(image, spinner) {
   setTimeout(function() {
     $(image).fadeIn("slow");
     // Remove spinner element
-    //image.parentNode.children[4].remove();
     spinner.remove();
   }, 1000);
   
@@ -16,8 +17,8 @@ function imgLoaded(image, spinner) {
 
 // Get list of photos
 function getPhotos(numImages, order, replace) {
-  const numPagesAvailable = 100; // im guessing
-  let randomImageIndex = Math.floor(Math.random() * numPagesAvailable);
+  let randomImageIndex = Math.floor(Math.random() * totalPages);
+  currPage = randomImageIndex;
   console.log('Getting photos...from page ' + randomImageIndex);
 
   // Client Access ID from your unsplash app dashboard
@@ -30,59 +31,78 @@ function getPhotos(numImages, order, replace) {
     order_by : order
   }
 
-  $.getJSON(`https://api.unsplash.com/photos/?${clientID}`, params, function(photoList) {
-    // Clear photo DOM elements
-    if (replace) {
-      $('#photos').html('');
-    }
+  // teststst
+  $.ajax({
+    type: "get",
+    url: `https://api.unsplash.com/photos/?${clientID}`,
+    crossDomain: true,
+    cache: false,
+    dataType: "json",
+    contentType: "application/json; charset=UTF-8",
+    data: params,
+    success: function(photoList, textStatus, xhr) {
+        var totalPhotos = xhr.getResponseHeader("X-Total");
+        totalPages = Math.floor(totalPhotos / numImages) - 1;
+        console.log('Total pages: ' + totalPages);
+        console.log('Total photos: ' + totalPhotos);
+        //console.log('Num photos this page: ' + xhr.getResponseHeader("X-Per-Page"));
 
-    $.each(photoList, function(index, value) {
-      //console.log(value);
-      
-      // Get attributes from json
-      var name = value.user.name;
-      var bio = value.user.bio;
-      if (bio === null) {bio = 'No Bio'}
-      var imageURL = value.urls.regular;
-      var dl = value.links.download;
-      var likes = value.likes;
-      //var w = Number(value.width);
-      //var h = Number(value.height);
-      var col = value.color;
+        // Clear photo DOM elements if refreshing
+        if (replace) {
+          $('#photos').html('');
+        }
 
-      // Make Elements
-      //
-      // Item Container
-      var itemEl = document.createElement('div');
-      itemEl.className = 'photo-item';
-      itemEl.title = dl;
-      itemEl.style.backgroundColor = col;
-      itemEl.innerHTML = 
-        `
-          <div class="sel-item"></div>
-          <h1 class="name" title="author">${name}</h1>
-          <h2 class="likes" title="likes">${likes}</h2>
-          <p class="bio" title="bio">${bio}</p>
-        `;
-      // Photo
-      var photoEl = document.createElement('div');
-      photoEl.className = 'photo-image';
-      photoEl.title = imageURL;
-      var imgEl = document.createElement('div');
-      imgEl.className = 'imgLoading';
-      $(photoEl).hide(); // Hide image to fadeIn later onLoad
-      photoEl.style.backgroundImage = 'url(' + imageURL + ')';
-      itemEl.appendChild(imgEl);
-      photoEl.addEventListener( "load", imgLoaded(photoEl, imgEl) ); // Listen for done loading
 
-      // Build DOM
-      //
-      // Add photo image to item container
-      itemEl.appendChild(photoEl);
-      // Add photo item to gallery container
-      $('#photos').append(itemEl);
-    });
-  });
+        $.each(photoList, function(index, value) {
+          // Get attributes from json
+          var avatar = value.user.profile_image.small;
+          var name = value.user.name;
+          var bio = value.user.bio;
+          if (bio === null) {bio = 'No Bio'}
+          var imageURL = value.urls.regular;
+          var dl = value.links.download;
+          var likes = value.likes;
+          var col = value.color;
+
+          // Make Elements
+          //
+          // Item Container
+          var itemEl = document.createElement('div');
+          itemEl.className = 'photo-item';
+          itemEl.title = dl;
+          itemEl.style.backgroundColor = col;
+          itemEl.innerHTML = 
+            `
+              <div class="sel-item"></div>
+              <div class="nameContainer">
+                <img src="${avatar}" class="avatar" height="26" width="26">
+                <h1 class="name" title="author">${name}</h1>
+              </div>
+              <h2 class="likes" title="likes">${likes}</h2>
+              <p class="bio" title="bio">${bio}</p>
+            `;
+          // Photo container
+          var photoEl = document.createElement('div');
+          photoEl.className = 'photo-image';
+          photoEl.title = imageURL;
+          var imgEl = document.createElement('div');
+          imgEl.className = 'imgLoading';
+          $(photoEl).hide(); // Hide image to fadeIn later onLoad
+          photoEl.style.backgroundImage = 'url(' + imageURL + ')';
+          itemEl.appendChild(imgEl);
+          photoEl.addEventListener( "load", imgLoaded(photoEl, imgEl) ); // Listen for done loading event to fade-in
+
+          // Build DOM
+          //
+          // Add photo image to item container
+          itemEl.appendChild(photoEl);
+          // Add photo item to gallery container
+          $('#photos').append(itemEl);
+        });
+    },
+    error: function (xhr, textStatus, errorThrown) {
+        console.log(errorThrown);
+    }});
 }
 
 // Link to GitHub
@@ -98,13 +118,14 @@ function goGitHub() {
 $(document).ready(function() {
   getPhotos(25, "latest", true);
   // Hide name, bio, etc onHover
+  let children = '.nameContainer, .bio, .likes';
   $(document).on('mouseenter', '.sel-item', function() {
     $(this).css('background-color', '#00000000');
-    $(this).parent().children('.name, .bio, .likes').hide();
+    $(this).parent().children(children).hide();
   });
   $(document).on('mouseleave', '.sel-item', function() {
     $(this).css('background-color', '#3838384d');
-    $(this).parent().children('.name, .bio, .likes').show();
+    $(this).parent().children(children).show();
   });
 });
 // Load more images if reached the bottom of page
